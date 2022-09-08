b3e2951921753044ce8c26cc9eb4e4211fdcf465
Iolanda Rodrigues <ioli.12@hotmail.com>
2022-09-08 13:06:16 -0300

    criando actions para ProjectsController

---
+++ DevFreela.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,66 @@
+﻿using DevFreela.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Api.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        //api/projects?query=net core
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            //Buscar ou filtrar todos os projetos
+            return Ok();
+        }
+
+        //api/projects/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //Buscar o projeto
+
+            return Ok();
+            //return NotFound() qd não encontrar nenhum elemento
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            //Cadastrar o projeto 
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        //api/projects/2
+        [HttpPut]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            //Atualizar o projeto
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar o projeto, se não existir, retornar NotFound()
+
+            //se existir, remover
+
+            return NoContent();
+        }
+
+
+    }
+}
