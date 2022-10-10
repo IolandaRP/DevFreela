131313d6302796c5778db8d81240b21e6afde532
Iolanda Rodrigues <ioli.12@hotmail.com>
2022-10-10 15:36:13 -0300

    configurando as entidas para tabelas

--- DevFreela.Core/Entities/User.cs
+++ DevFreela.Core/Entities/User.cs
@@ -16,6 +16,7 @@ namespace DevFreela.Core.Entities
         public List<UserSkill> Skills { get; private set; }
         public List<Project> OwnedProjects { get; private set; }
         public List<Project> FreelanceProjects { get; private set; }
+        public List<ProjectComment> Comments { get; private set; }
 
         public User(string fullName, string email, DateTime birthDate)
         {
@@ -28,6 +29,7 @@ namespace DevFreela.Core.Entities
             Skills = new List<UserSkill>();
             OwnedProjects = new List<Project>();
             FreelanceProjects = new List<Project>();
+            Comments = new List<ProjectComment>();
         }
     }
 }
