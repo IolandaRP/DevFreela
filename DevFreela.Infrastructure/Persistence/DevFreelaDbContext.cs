1de13664636c09c07e75a4d2b23efd78415d6863
Iolanda Rodrigues <ioli.12@hotmail.com>
2022-10-10 11:24:49 -0300

    criando o DbContext e DbSet's

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,35 +8,16 @@ using System.Threading.Tasks;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
 
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>()
-            {
-                new Project ("Meu projeto APSNET Core 1", "Minha descrição de Projeto 1", 1, 1, 10000),
-                new Project ("Meu projeto APSNET Core 2", "Minha descrição de Projeto 2", 1, 1, 20000),
-                new Project ("Meu projeto APSNET Core 3", "Minha descrição de Projeto 3", 1, 1, 30000)
-            };
-
-            Users = new List<User>()
-            {
-                new User("Iolanda Rorigues", "iolandarodrigues270@gmail.com", new DateTime(1995,4,4)),
-                new User("Robert C Martin", "robert@gmail.com", new DateTime(1952,1,2)),
-                new User("Luis Felipe", "luisdev@luisdev.com.br", new DateTime(1992,1,1))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".Net Core"),
-                new Skill("C#"),
-                new Skill("SQL Server")
-            };
         }
 
 
